439b008595770524280ef23e7d9991e02701642e
asd3638 <asd36384@gmail.com>
2022-01-26 22:52:44 +0900

    cors

--- src/main/java/SKRookie/moamoa/config/WebConfig.java
+++ src/main/java/SKRookie/moamoa/config/WebConfig.java
@@ -1,6 +1,7 @@
 package SKRookie.moamoa.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://moa-moa.kr", "https://moa-moa.kr:3000");
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());
     }
 }
