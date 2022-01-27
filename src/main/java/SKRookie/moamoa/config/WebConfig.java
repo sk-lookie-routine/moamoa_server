dc41c20c6129afdd3071b0c74b917c213d41a1df
asd3638 <asd36384@gmail.com>
2022-01-27 14:52:33 +0900

    change cors variables to properties

--- src/main/java/SKRookie/moamoa/config/WebConfig.java
+++ src/main/java/SKRookie/moamoa/config/WebConfig.java
@@ -1,5 +1,6 @@
 package SKRookie.moamoa.config;
 
+import SKRookie.moamoa.config.properties.CorsProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,16 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    CorsProperties corsProperties = new CorsProperties();
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://moa-moa.kr")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name());
+            registry.addMapping("/**")
+                    .allowedOrigins(corsProperties.getAllowedOrigins())
+                    .allowedMethods(
+                            HttpMethod.GET.name(),
+                            HttpMethod.HEAD.name(),
+                            HttpMethod.POST.name(),
+                            HttpMethod.PUT.name(),
+                            HttpMethod.DELETE.name())
+                    .allowedHeaders(corsProperties.getAllowedHeaders());
     }
 }
